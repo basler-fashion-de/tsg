2f09a9193462ff3cfdf313aeada57b101dce9f92
Dennis Ottenbacher <mail@dennisottenbacher.de>
2018-02-19 17:18:00 +0100

    vsc-Dateien von der Kopie ausgeschlossen

--- Services/System/Common/CodebaseDuplicationService.php
+++ Services/System/Common/CodebaseDuplicationService.php
@@ -14,22 +14,27 @@ class CodebaseDuplicationService
 
     private $blackList;
 
+    private $vcsFiles;
+
     private $skipHidden;
 
     public function __construct(\Enlight_Components_Snippet_Manager $snippets, ConfigService $filesConfigService)
     {
         $this->snippets = $snippets;
         $this->blackList = $filesConfigService->get('files.blacklist.file', true);
+        $this->vcsFiles = (
+            isset($filesConfigService->get('files.blacklist.@attributes')['vcsFiles']) &&
+            $filesConfigService->get('files.blacklist.@attributes')['vcsFiles'] === 'true'
+        );
+
         $this->skipHidden = (
             isset($filesConfigService->get('files.blacklist.@attributes')['hiddenFiles']) &&
             $filesConfigService->get('files.blacklist.@attributes')['hiddenFiles'] === 'true'
-            );
+        );
     }
 
     public function duplicateCodeBase($sourcePath, $destinationPath, $exceptions = [])
     {
-        $exceptions = array_merge($this->blackList, $exceptions);
-
         if (!@mkdir($destinationPath) && !is_dir($destinationPath)) {
             throw new SystemFileSystemException(
                 sprintf($this->snippets->getNamespace('blauband/ocs')->get('destinationPathNotCreated'), $destinationPath)
@@ -41,21 +46,30 @@ class CodebaseDuplicationService
         return true;
     }
 
-    public function removeDuplicatedCodebase($path){
+    public function removeDuplicatedCodebase($path)
+    {
         exec("rm -r -d -f $path", $output, $return);
     }
 
-    private function copyWithTar($sourcePath, $destinationPath, $exceptions){
-        $exceptionsString = '';
+    private function copyWithTar($sourcePath, $destinationPath, $exceptions)
+    {
+        $exceptions = array_merge($this->blackList, $exceptions);
 
-        if(!empty($exceptions)){
-            $exceptionsString .= ' --exclude='.implode(' --exclude=', $exceptions);
-            $exceptionsString = str_replace($sourcePath.'/', '', $exceptionsString);
+        $exceptionsString = ' --exclude=' . $destinationPath;
+
+        if (!empty($exceptions)) {
+            $exceptionsString .= ' --exclude=' . implode(' --exclude=', $exceptions);
         }
 
-        if($this->skipHidden){
+        if ($this->skipHidden) {
             $exceptionsString .= " --exclude='.[^/]*' ";
         }
+
+        if ($this->vcsFiles) {
+            $exceptionsString .= ' --exclude-vcs ';
+        }
+
+        $exceptionsString = str_replace($sourcePath . '/', '', $exceptionsString);
 
         $command = "(cd $sourcePath && tar cmf - $exceptionsString .) | (cd $destinationPath && tar xvmf - )";
         exec($command);
